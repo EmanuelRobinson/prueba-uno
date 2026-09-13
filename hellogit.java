9f920cbcb9a0a71a8e41df9ea3cb7510f4f096bd
Emarobins <emarobins.oh@gmail.com>
2026-09-13 11:34:01 -0500

    Este es mi primer commit con el curso de MoureDev.

---
+++ hellogit.java
@@ -0,0 +1,30 @@
+System.out.println("Hola github");
+/*
+Que cositas estoy aprendiendo hasta ahora con git
+
+ya vi donde se descarga desde su misma pagina principal "GIT"
+
+como ver si esta instalado colocando en mi powershel git o git --version
+
+comando global de configuracion ya sea nombre y email
+
+git --global user.name "Emarobins"
+git --global user.email "Emarobins"
+
+vi comandos para navegar dentro del powershel usando git 
+
+ls -> para visualizar todo el contenido de lo que hay en mi carpeta
+cd -> para acceder a la carpeta
+cd.. -> para retroceder de la carpeta
+pwd -> para visualizar la ruta en la que me encuentro
+mkdir -> para crear una carpeta con el nombre que deseo (mkdir "nueva carpeta")
+
+cls -> para limpiar el exceso de codigo o comandos realizados dentro de mi powershell.
+
+git init -> para incializar mi proyecto dentro de git
+git branch -m main -> para cambiar la rama master a main la cual es la mas usada o comun por el nombre que tiene por defecto.
+git status -> para ver los diferentes estados por lo que ah pasado mi rama.
+
+
+*/
+
