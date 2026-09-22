6e72ed897eed735307deffef2911261803316185
Emarobins <emarobins.oh@gmail.com>
2026-09-22 13:19:57 -0500

    primer commit en repo de github

--- hellogit.java
+++ hellogit.java
@@ -62,6 +62,7 @@ interesante acabo de ver como crear nuevas ramas y como cambiarme a la rama que 
 git branch "nombre de la rama sin comillas" -> se crea una nueva rama con el nombre que sea de nuestro gusto
 git switch "nombre de la rama sin comillas" -> para cambiar a la rama que deseemos mencionando el nombre jiji dependiendo de como cambiemos se aplicara a los cambios, se usa mayormente de manera local.
 
+esto son nuevos apuntes subideos a un repo
 
 */
 println("New hellow git dskadaldaslkdakl");
